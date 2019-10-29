7591d89cbe3977e394d45dc72b19185389f654db
David Banks <amoebae@gmail.com>
2019-10-29 17:26:48 +0000

    store is created successfully

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,40 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+
+interface MyState {
+    counter: number;
+}
+
+export const INCREMENT = 'INCREMENT';
+
+interface IncrementAction {
+}
+
+// This could be a sum type
+type MyActionTypes = IncrementAction;
+
+// Don't modify the state
+
+// The type of the state should be T | undefined.
+// In the case of the tutorial, they use a too-clever-by-half thing to infer
+// the type correctly.
+function myReducer(state: MyState | undefined, action: MyActionTypes): MyState {
+    if (state === undefined) {
+        return { counter: 0 };
+    }
+
+    return state;
+}
+
+
+
+
+// the root reducer must be treated specially...
+const store = createStore(myReducer);
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
