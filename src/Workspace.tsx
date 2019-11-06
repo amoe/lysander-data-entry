0413b2879e83f4918719b6382ee4d29ce11b6965
David Banks <amoebae@gmail.com>
2019-11-06 15:32:57 +0000

    material formik ui

--- src/Workspace.tsx
+++ src/Workspace.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import actionCreators from './action-creators';
 import { FullStateTree, IncrementAction } from './interfaces';
-import { Formik } from 'formik';
+import { Formik, Form, Field, FormikProps } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { MenuItem, Button } from '@material-ui/core';
 
 
 function mapStateToProps(state: FullStateTree) {
@@ -23,6 +25,61 @@ interface AppProps {
     demoQuery: () => typeof actionCreators.demoQuery;
 }
 
+const ranges = [
+    {
+        value: 'none',
+        label: 'None',
+    },
+    {
+        value: '0-20',
+        label: '0 to 20',
+    },
+    {
+        value: '21-50',
+        label: '21 to 50',
+    },
+    {
+        value: '51-100',
+        label: '51 to 100',
+    },
+];
+
+interface MyFormProps {
+    select: string;
+}
+
+const MyFormComponent = (props: FormikProps<MyFormProps>) => {
+    return (
+        <Form>
+            <Field type="text"
+                name="select"
+                label="With Select"
+                select
+                variant="standard"
+                helperText="Please select Range"
+                margin="normal"
+                component={TextField}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            >
+                {ranges.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </Field>
+
+            <Button variant="contained"
+                color="primary"
+                onClick={props.submitForm}>
+                Stumbit
+            </Button>
+        </Form>
+    );
+};
+
+
 class MyComponent extends React.Component<AppProps> {
     componentDidMount() {
         console.log("component mounted");
@@ -31,9 +88,19 @@ class MyComponent extends React.Component<AppProps> {
     render() {
         return (
             <div>
-                <p>Counter value: {this.props.counter}</p>
-                <button onClick={(e) => this.props.increment()}>Increment</button>
-                <button onClick={(e) => this.props.demoQuery()}>Async increment</button>
+                <div>
+                    <p>Counter value: {this.props.counter}</p>
+                    <button onClick={(e) => this.props.increment()}>Increment</button>
+                    <button onClick={(e) => this.props.demoQuery()}>Async increment</button>
+                </div>
+
+                <Formik initialValues={
+                    { select: 'none' }
+                } onSubmit={(values) => {
+                    console.log("values are %o", values);
+                }} component={MyFormComponent}>
+                </Formik>
+
             </div>
         );
     }
