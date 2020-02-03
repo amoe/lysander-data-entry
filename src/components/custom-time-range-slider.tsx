5db31ad4e3f877e9e735f25aeb85576eab207b4c
David Banks <amoebae@gmail.com>
2020-02-03 16:29:41 +0000

    replace the time range slider

--- src/components/custom-time-range-slider.tsx
+++ src/components/custom-time-range-slider.tsx
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { addDataToMap } from 'kepler.gl/actions';
 import { FullStateTree, IncrementAction } from '../interfaces';
 import actionCreators from '../action-creators';
-import { SidebarFactory } from 'kepler.gl/components';
+import { SidebarFactory, RangeSlider, TimeSliderMarker } from 'kepler.gl/components';
+import throttle from 'lodash.throttle';
 
 function mapStateToProps(state: FullStateTree) {
     return {
@@ -28,9 +29,21 @@ function CustomTimeRangeSliderFactory(
 ) {
     return connect(mapStateToProps, mapDispatchToProps)(
         class CustomTimeRangeSlider extends React.Component<any, any> {
+            _sliderThrottle: any;
+
+            _sliderUpdate = (args: any) => {
+                this._sliderThrottle.cancel();
+                this._sliderThrottle(args);
+            };
+
             constructor(props: any) {
                 super(props);
                 console.log("inside constructor");
+                this._sliderThrottle = throttle(
+                    (...value: any[]) => this.props.onChange(...value),
+                    20
+                );
+
             }
 
             componentDidMount() {
@@ -38,8 +51,25 @@ function CustomTimeRangeSliderFactory(
 
             
             render() {
+                const { domain, value, isEnlarged } = this.props;
+
                 return (
+                    <div>
                     <h1>CustomTimeRangeSliderFactory</h1>
+
+                    <RangeSlider range={domain}
+                                 value0={value[0]}
+                                 value1={value[1]}
+                                 histogram={this.props.histogram}
+                                 lineChart={this.props.lineChart}
+                                 plotType={this.props.plotType}
+                                 isEnlarged={isEnlarged}
+                                 showInput={false}
+                                 step={this.props.step}
+                                 onChange={this._sliderUpdate}
+                                 xAxis={TimeSliderMarker}
+                    />
+                    </div>
                 );
             }
         }
