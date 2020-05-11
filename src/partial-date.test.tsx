92263540152adbec2cf44158026d66e28ac21dd8
David Banks <amoebae@gmail.com>
2020-05-11 11:52:09 +0100

    sort functions

--- src/partial-date.test.tsx
+++ src/partial-date.test.tsx
@@ -1,6 +1,36 @@
-import {PartialDate} from './partial-date';
+import {PartialDate, comparePartialDates} from './partial-date';
 
 it('constructs a year', () => {
     const d = new PartialDate(1940);
     expect(d.year).toEqual(1940);
 });
+
+it('constructs a year-and-month', () => {
+    const d = new PartialDate(1940, 6);
+    expect(d.year).toEqual(1940);
+    expect(d.month).toEqual(6);
+    expect(d.day).toBeUndefined();
+});
+
+it('constructs a year-and-month-and-day', () => {
+    const d = new PartialDate(1940, 6, 9);
+    expect(d.year).toEqual(1940);
+    expect(d.month).toEqual(6);
+    expect(d.day).toEqual(9);
+});
+
+it('allows ordering partial dates', () => {
+    const d1 = new PartialDate(1940);
+    const d2 = new PartialDate(1940, 6);
+    const d3 = new PartialDate(1940, 6, 9);
+
+    const allDates = [d3, d2, d1];
+    
+    allDates.sort(comparePartialDates);
+
+    // Unknown points should be filled with zeros, and compared
+    // based on the start-point of their respective date-ranges.
+    expect(allDates).toEqual([d1, d2, d3]);
+});
+
+
--- src/partial-date.ts
+++ src/partial-date.ts
@@ -1,3 +1,9 @@
+//    If compareFunction(a, b) returns less than 0, sort a to an index lower than b (i.e. a comes first).
+//    If compareFunction(a, b) returns 0, leave a and b unchanged with respect to each other, but sorted with respect to all different elements. Note: the ECMAscript standard does not guarantee this behavior, thus, not all browsers (e.g. Mozilla versions dating back to at least 2003) respect this.
+//    If compareFunction(a, b) returns greater than 0, sort b to an index lower than a (i.e. b comes first).
+//    compareFunction(a, b) must always return the same value when given a specific pair of elements a and b as its two arguments. If inconsistent results are returned, then the sort order is undefined.
+
+
 class PartialDate {
     year: number;
     month: number | undefined;
@@ -10,4 +16,10 @@ class PartialDate {
     }
 }
 
-export {PartialDate};
+
+function comparePartialDates(a: PartialDate, b: PartialDate) {
+    
+}
+
+
+export {PartialDate, comparePartialDates};
