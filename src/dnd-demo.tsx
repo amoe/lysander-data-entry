915f65eb5890dfccfe1063a65eb3b51102eba918
David Banks <amoebae@gmail.com>
2020-05-12 16:31:37 +0100

    refactor list into state

--- src/dnd-demo.tsx
+++ src/dnd-demo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     DragDropContext, Droppable, Draggable,
     DropResult, DroppableProvided, DroppableStateSnapshot, DraggableRubric, 
@@ -8,46 +8,55 @@ import {
 interface AppProps {
 };
 
-function onDragEnd(result: DropResult, provided: ResponderProvided) {
-    console.log("ondragend called");
-}
-
 function makeDraggableChildren(value: string) {
     return (provided: DraggableProvided,
             snapshot: DraggableStateSnapshot,
             rubric: DraggableRubric) => 
                 <li ref={provided.innerRef} 
                      {...provided.dragHandleProps}
-                     {...provided.draggableProps}>{value}</li>
+                     {...provided.draggableProps}>{value}</li>;
 }
 
-function droppableChildren(
-    provided: DroppableProvided, snapshot: DroppableStateSnapshot
-) {
-    const items = ['fry', 'bender', 'leela'];
-
-    return (
-        <ul ref={provided.innerRef}>
-          {items.map((value, i) => (
-              <Draggable draggableId={i.toString()}
-                         index={i}
-                         key={i}>
-                {makeDraggableChildren(value)}
-              </Draggable>
-          ))}
-          {provided.placeholder}
-        </ul>
-    )
+function makeDroppableChildren(items: string[]) {
+    return (provided: DroppableProvided, 
+            snapshot: DroppableStateSnapshot) =>
+                <ul ref={provided.innerRef}>
+                  {items.map((value, i) => (
+                      <Draggable draggableId={i.toString()}
+                                 index={i}
+                                 key={i}
+                                 isDragDisabled={false}>
+                        {makeDraggableChildren(value)}
+                      </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </ul>;
 }
 
 export function DndDemo(props: AppProps) {
+    const [items, setItems] = useState(['fry', 'bender', 'leela']);
+
+    const onDragEnd = (result: DropResult, provided: ResponderProvided) => {
+        if (!result.destination)  return;    // invalid drop
+
+        const startIndex = result.source.index;
+        const endIndex = result.destination.index;
+
+        // Destructure because we know we always have 1 item.
+        const [removed] = items.splice(startIndex, 1);
+        items.splice(endIndex, 0, removed);
+        setItems(items);
+
+        console.log("start index is %o, end index is %o", startIndex, endIndex);
+    };
+
     return (
         <div>
           <h1>Hello</h1>
 
           <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="main">
-              {droppableChildren}
+            <Droppable droppableId="main" isDropDisabled={false}>
+              {makeDroppableChildren(items)}
             </Droppable>
           </DragDropContext>
         </div>
