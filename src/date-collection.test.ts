d05cd3deb7c4e7182e242c3ab1764b4adda96b14
David Banks <amoebae@gmail.com>
2020-05-14 10:32:27 +0100

    red test

--- src/date-collection.test.ts
+++ src/date-collection.test.ts
@@ -1,7 +1,18 @@
 import {DateCollection} from './date-collection';
+import {PartialDate} from './partial-date';
 
 it('works', () => {
-    expect(2 + 2).toBe(4);
+    const ordering: PartialDate[] = [
+        new PartialDate(1939),
+        new PartialDate(1939, 1),
+        new PartialDate(1939, 11, 20),
+        new PartialDate(1940)
+    ];
+
+    const coll = DateCollection.fromArray(ordering);
+
+//    expect(
+
 });
 
 
