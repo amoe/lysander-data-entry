8259368a2ecd55134911b7c58bf9f030c0cbdc95
David Banks <amoebae@gmail.com>
2020-05-15 16:22:57 +0100

    fix layout

--- src/location-authoring-demo.tsx
+++ src/location-authoring-demo.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import {Form, Input} from 'antd';
 
 export function LocationAuthoringDemo() {
+    const layout = {
+        //labelCol: { span: 8 },
+        //wrapperCol: { span: 16 },
+    };
+
     return (
         <div>
           <h1>Location authoring demo</h1>
 
-          <Form>
-          <Form.Item label="Username" name="username">
-            <Input/>
-          </Form.Item>
+          <Form {...layout}>
+            <Form.Item label="Username" name="username">
+              <Input/>
+            </Form.Item>
           </Form>
 
         </div>
