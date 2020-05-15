9ca91fb51a785f67443227c78a60039bac4f4a8d
David Banks <amoebae@gmail.com>
2020-05-15 10:45:37 +0100

    listing out dates

--- src/date-authoring-demo.tsx
+++ src/date-authoring-demo.tsx
@@ -1,6 +1,8 @@
-import React, {useState, ChangeEvent} from 'react';
+import React, {useState, ChangeEvent, MouseEvent} from 'react';
 import {InputNumber, Checkbox} from 'antd';
 import {Button, Form, Input} from 'antd';
+import {PartialDate} from './partial-date';
+import {clone} from 'lodash';
 
 // not sure how to type the onchange handler
 function OptionalNumber(props: {value: number, onChange: any, label: string}) {
@@ -23,12 +25,25 @@ export function DateAuthoringDemo() {
     const [year, setYear] = useState(1939);
     const [month, setMonth] = useState(1);
 
+    const [dates, setDates] = useState<PartialDate[]>([]);
+
+    const addDate = (e: MouseEvent) => {
+        setDates([...dates, new PartialDate(1939)]);
+    };
+
     return (
         <div>
           <h1>Date authoring demo</h1>
           
           <OptionalNumber value={year} onChange={setYear} label="Year"/>
           <OptionalNumber value={month} onChange={setMonth} label="Month"/>
+
+
+          <ul>
+            {dates.map((x, i) => <li>{x.toString()}</li>)}
+          </ul>
+
+          <button onClick={addDate}>Add</button>
         </div>
     );
 }
