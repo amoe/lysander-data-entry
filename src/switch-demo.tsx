b004bf9322e0f95fbeab035c45f8c3c70ef4a78d
David Banks <amoebae@gmail.com>
2020-05-15 15:57:43 +0100

    demonstrate conditional rendering using switch

--- src/switch-demo.tsx
+++ src/switch-demo.tsx
@@ -1,6 +1,34 @@
 import React, {useState} from 'react';
 
 
+function ModeZero() {
+    return (
+        <div>
+        <h2>This is mode zero.</h2>
+        </div>
+    );
+}
+
+function ModeOne() {
+    return (
+        <div>
+          <h2>This is mode one.</h2>
+        </div>
+    );
+}
+
+function SwitchedMode(props: {selectedUi: number}) {
+    switch (props.selectedUi) {
+        case 0:
+            return <ModeZero/>;
+        case 1:
+            return <ModeOne/>;
+        default:
+            throw new Error("no");
+    }
+}
+
+
 export function SwitchDemo() {
     const [selectedUi, setSelectedUi] = useState(0);  // should be enum
 
@@ -16,10 +44,10 @@ export function SwitchDemo() {
         <div>
           <h1>Foo</h1>
 
-          <p>UI is{selectedUi}</p>
+          <p>UI is {selectedUi}</p>
 
           <button onClick={toggleUi}>Toggle ui</button>
-
+          <SwitchedMode selectedUi={selectedUi}/>
         </div>
     );
 }
