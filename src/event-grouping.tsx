ba9c25bbfc2a0d8d47004eb3257b884956eb7b42
David Banks <amoebae@gmail.com>
2020-05-18 16:38:09 +0100

    grouping demo

--- src/event-grouping.tsx
+++ src/event-grouping.tsx
@@ -46,9 +46,16 @@ function isSingleEvent(x: any): x is SingleEvent {
 
 type MoveHandler = (eventId: string) => void;
 
+
 function SequenceMember(props: {value: SequenceMember, viewMode: ViewMode, onMove: MoveHandler}) {
     if (isEventGroup(props.value)) {
-        return <h2>A group</h2>;
+        // What's the key in this case???
+        return (
+            <div>
+              <h2>Group Begin</h2>
+              <h2>Group End</h2>
+            </div>
+        );        
     } else if (isSingleEvent(props.value)) {
         // Must re-bind it so that the type guard applies, otherwise we can't use
         // it in callbacks.
@@ -79,8 +86,10 @@ export function EventGrouping() {
     const events: EventSequence = [
         {id: 'e1', description: 'Bartholomew I of Constantinople issues a formal decree granting independence to the Orthodox Church of Ukraine from the Russian Orthodox Church.', date: '2019-01-05'},
         {id: 'e2', description: 'Venezuelan presidential crisis: President Maduro severs diplomatic ties with Colombia as humanitarian aid attempts to enter the country across the border.', date: '2019-02-23'},
+        [
         {id: 'e3', description: "Europe's antitrust regulators fine Google 1.49 billion euros ($1.7 billion) for freezing out rivals in the online advertising business. The ruling brings to nearly $10 billion the fines imposed against Google by the European Union.", date: '2019-03-20'},
         {id: 'e4', description: 'A series of bomb attacks occur at eight locations in Sri Lanka, including three churches, four hotels and one housing complex in Colombo, on Easter Sunday, leaving 259 people dead and over 500 injured. It is the first major terrorist attack in the country since the Sri Lankan Civil War ended in 2009.', date: '2019-04-21'}
+        ]
     ];
 
     const doMove = (eventId: string) => {
