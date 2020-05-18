56dd871acd606375d863acdf568ac48cac2bc4ff
David Banks <amoebae@gmail.com>
2020-05-18 11:56:39 +0100

    give an id to the events

--- src/event-grouping.tsx
+++ src/event-grouping.tsx
@@ -9,11 +9,14 @@ function GroupingIcon() {
 
 export function EventGrouping() {
     const events = [
-        {description: 'Bartholomew I of Constantinople issues a formal decree granting independence to the Orthodox Church of Ukraine from the Russian Orthodox Church.', date: '2019-01-05'},
-        {description: 'Venezuelan presidential crisis: President Maduro severs diplomatic ties with Colombia as humanitarian aid attempts to enter the country across the border.', date: '2019-02-23'},
-        {description: "Europe's antitrust regulators fine Google 1.49 billion euros ($1.7 billion) for freezing out rivals in the online advertising business. The ruling brings to nearly $10 billion the fines imposed against Google by the European Union.", date: '2019-03-20'},
-        {description: 'A series of bomb attacks occur at eight locations in Sri Lanka, including three churches, four hotels and one housing complex in Colombo, on Easter Sunday, leaving 259 people dead and over 500 injured. It is the first major terrorist attack in the country since the Sri Lankan Civil War ended in 2009.', date: '2019-04-21'}
+        {id: 'e1', description: 'Bartholomew I of Constantinople issues a formal decree granting independence to the Orthodox Church of Ukraine from the Russian Orthodox Church.', date: '2019-01-05'},
+        {id: 'e2', description: 'Venezuelan presidential crisis: President Maduro severs diplomatic ties with Colombia as humanitarian aid attempts to enter the country across the border.', date: '2019-02-23'},
+        {id: 'e3', description: "Europe's antitrust regulators fine Google 1.49 billion euros ($1.7 billion) for freezing out rivals in the online advertising business. The ruling brings to nearly $10 billion the fines imposed against Google by the European Union.", date: '2019-03-20'},
+        {id: 'e4', description: 'A series of bomb attacks occur at eight locations in Sri Lanka, including three churches, four hotels and one housing complex in Colombo, on Easter Sunday, leaving 259 people dead and over 500 injured. It is the first major terrorist attack in the country since the Sri Lankan Civil War ended in 2009.', date: '2019-04-21'}
     ];
+
+    // Note that it's clear that if we use index as the key, reordering is not
+    // going to work.
 
 
     return (
@@ -23,7 +26,7 @@ export function EventGrouping() {
 
             {events.map((e, i) => {
                 return (
-            <div key={i} className="event-summary">
+            <div key={e.id} className="event-summary">
               <div className="event-description">
                 {e.description}
               </div>
