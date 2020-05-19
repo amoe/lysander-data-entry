513aa29c9791391d8d9d93616e4a649dfd91e59f
David Banks <amoebae@gmail.com>
2020-05-19 16:29:37 +0100

    factoring to components

--- src/sequence-member-renderer.tsx
+++ src/sequence-member-renderer.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { EventSequence, FlightEvent, SequenceMember, EventGroup } from './event-sequence';
 import { ViewMode, MoveHandler } from './interfaces2';
 
+function FlightEventView() {
+    return <div><h2>Simple flightevent</h2></div>;
+}
+
+function EventGroupView() {
+    return <div><h2>An event group</h2></div>;
+}
+
 export function SequenceMemberView(
     props: {value: SequenceMember, viewMode: ViewMode, onMove: MoveHandler}
 ) {
     const val = props.value;
 
     if (val instanceof FlightEvent) {
-        return <div><h2>Simple flightevent</h2></div>;
+        return <FlightEventView />
     } else if (val instanceof EventGroup) {
-        return <div><h2>An event group</h2></div>
+        return <EventGroupView />
     } else {
         throw new Error("unexpected sequence member type");
     }
