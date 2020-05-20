65aa068f65c74ecb0030fb047c09e650ff8ca19b
David Banks <amoebae@gmail.com>
2020-05-20 16:25:14 +0100

    layout

--- src/theme-panel.tsx
+++ src/theme-panel.tsx
@@ -6,18 +6,35 @@ const { Header, Footer, Sider, Content } = Layout;
 
 function Foo() {
     return (
-        <div>
-        <Form.Item label="Theme">
-          <Select>
-            <Select.Option value="lt">Event</Select.Option>
-            <Select.Option value="eq">Person</Select.Option>
-            <Select.Option value="gt">Organization</Select.Option>
-          </Select>
-        </Form.Item>
-        <Button icon={<PlusOutlined/>}></Button>
-        </div>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Theme">
+              <Select>
+                <Select.Option value="lt">Event</Select.Option>
+                <Select.Option value="eq">Person</Select.Option>
+                <Select.Option value="gt">Organization</Select.Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6} offset={3}>
+            <Button icon={<PlusOutlined/>}></Button>
+          </Col>
+        </Row>
     )
         
+}
+
+function Bar() {
+    return (
+        <Row>
+          <Col span={12}>
+            <LoremIpsum/>
+          </Col>
+          <Col span={12}>
+            <LoremIpsum/>
+          </Col>
+        </Row>
+    );
 }
 
 export function ThemePanel() {
@@ -26,7 +43,7 @@ export function ThemePanel() {
           <Content>
             <Row>
               <Col span={12} offset={6}>
-                <LoremIpsum />
+                <Foo />
               </Col>
             </Row>
           </Content>
