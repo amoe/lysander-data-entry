37e223c3fbf9a728ffa9f52a3096d90b42ef169f
David Banks <amoebae@gmail.com>
2020-08-11 17:00:47 +0100

    implement splitting

--- src/group-and-move-demo.tsx
+++ src/group-and-move-demo.tsx
@@ -2,6 +2,9 @@ import React, {useReducer, useState} from 'react';
 import {cloneDeep} from 'lodash';
 import uuidv4 from 'uuid/v4';
 
+// You can only split a group.  That means that splitting requires two parameters:
+// index within the EventList and index within the group.  If it is not a group,
+// an error should be thrown.
 
 enum ListItemType {
     SINGLE_EVENT = 'singleEvent',
@@ -17,14 +20,16 @@ type EventList = Array<EventItem>;
 enum ActionType {
     ADD_ITEM = 'addItem',
     MOVE_ITEM = 'moveItem',
-    CONNECT_TO_ADJACENT_ITEM = 'connectToAdjacentItem'
+    CONNECT_TO_ADJACENT_ITEM = 'connectToAdjacentItem',
+    SPLIT_GROUP_AT_INDEX = 'splitGroupAtIndex'
 };
 
 
 type Action = 
   {type: ActionType.ADD_ITEM, content: EventContent}
   | {type: ActionType.MOVE_ITEM, sourcePosition: number, targetPosition: number}
-  | {type: ActionType.CONNECT_TO_ADJACENT_ITEM, firstItem: number};
+  | {type: ActionType.CONNECT_TO_ADJACENT_ITEM, firstItem: number}
+  | {type: ActionType.SPLIT_GROUP_AT_INDEX, itemIndex: number, groupOffset: number};
 
 function contentAsArray(item: EventItem): EventContent[] {
     switch (item.type) {
@@ -37,6 +42,81 @@ function contentAsArray(item: EventItem): EventContent[] {
     }
 }
 
+
+function splitGroupAtIndex(state: EventList, itemIndex: number, groupOffset: number) {
+    const group = state[itemIndex];
+    if (group.type !== ListItemType.GROUP) {
+        throw new Error("cannot split a non group");
+    }
+
+    const currentContent = group.groupContent;
+
+
+    // For a  group of 2 items.
+    // If we split at offset 0, it's invalid, and would remain the same.
+    // If we split at offset 1, we split the group in half.
+    // If we split at offset 2, it's invalid and would remain the same.
+
+    // For a group of 3 items:
+    // offset 0 = invalid
+    // offset 1 = 1 single item, 1 group of 2
+    // offset 2 = 1 group of 2, 1 single item
+    // offset 3 = invalid
+
+    if (groupOffset <= 0) {
+        throw new Error("invalid split attempt: index is too low");
+    }
+
+    if (groupOffset >= currentContent.length) {
+        throw new Error("invalid split attempt: index is too high");
+    }
+
+    const fore = [];
+    const aft = [];
+
+    for (var i = 0; i < currentContent.length; i++) {
+        const item = currentContent[i];
+
+        if (i < groupOffset) {
+            fore.push(item);
+        } else {
+            aft.push(item);
+        }
+    }
+
+    var foreItem: EventItem;
+    if (fore.length === 1) {
+        foreItem = {
+            type: ListItemType.SINGLE_EVENT,
+            content: fore[0],
+            id: uuidv4()    // again not sure this is right
+        };
+    } else {
+        foreItem = {
+            type: ListItemType.GROUP,
+            groupContent: fore,
+            id: group.id    // keep id of original group for first
+        }
+    }
+
+    var aftItem: EventItem;
+    if (aft.length === 1) {
+        aftItem = {
+            type: ListItemType.SINGLE_EVENT,
+            content: aft[0],
+            id: uuidv4()    // XXX, same issue as before
+        };
+    } else {
+        aftItem = {
+            type: ListItemType.GROUP,
+            groupContent: aft,
+            id: uuidv4()    // always gen a new id for the second list
+        }
+    }
+
+    state.splice(itemIndex, 1, foreItem, aftItem);
+    return state;
+}
 
 function connectToAdjacentItem(state: EventList, firstItem: number) {
     const targetItem = state[firstItem];
@@ -77,6 +157,8 @@ function reduceEventList(state: EventList, action: Action): EventList {
             return newState;
         case ActionType.CONNECT_TO_ADJACENT_ITEM:
             return connectToAdjacentItem(cloneDeep(state), action.firstItem);
+        case ActionType.SPLIT_GROUP_AT_INDEX:
+            return splitGroupAtIndex(cloneDeep(state), action.itemIndex, action.groupOffset);
         default:
             throw new Error("no");
     }
@@ -107,6 +189,9 @@ export function GroupAndMoveDemo() {
     const [targetPosition, setTargetPosition] = useState<number>(0);
     const [firstItemToConnect, setFirstItemToConnect] = useState<number>(0);
 
+    const [splitItemIndex, setSplitItemIndex] = useState<number>(0);
+    const [splitGroupOffset, setSplitGroupOffset] = useState<number>(0);
+
     const handleTargetPositionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTargetPosition(parseInt(e.target.value));
     };
@@ -117,6 +202,14 @@ export function GroupAndMoveDemo() {
 
     const handleFirstItemToConnectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFirstItemToConnect(parseInt(e.target.value));
+    };
+
+    const handleSplitItemIndexChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSplitItemIndex(parseInt(e.target.value));
+    };
+
+    const handleSplitGroupOffsetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSplitGroupOffset(parseInt(e.target.value));
     };
 
     return (
@@ -158,11 +251,24 @@ export function GroupAndMoveDemo() {
             <label>First item
               <input type="number"
                      value={firstItemToConnect}
-                     onChange={handleFirstItemToConnectChange}/>
+        onChange={handleFirstItemToConnectChange}/>
             </label>
             <button onClick={() => dispatch({type: ActionType.CONNECT_TO_ADJACENT_ITEM, firstItem: firstItemToConnect})}>Connect to adjacent item</button>
           </div>
 
+          <div>
+            <label>Item index
+              <input type="number"
+                     value={splitItemIndex} onChange={handleSplitItemIndexChange}/>
+            </label>
+
+            <label>Group offset
+              <input type="number"
+                     value={splitGroupOffset} onChange={handleSplitGroupOffsetChange}/>
+            </label>
+
+            <button onClick={() => dispatch({type: ActionType.SPLIT_GROUP_AT_INDEX, itemIndex: splitItemIndex, groupOffset: splitGroupOffset})}>Split group</button>
+          </div>
         </div>
     );
 }
