dab011d548cdeb114262ac51395d22a4de0f9dc5
David Banks <amoebae@gmail.com>
2020-08-13 16:29:44 +0100

    working with dnd

--- src/react-dnd-demo.tsx
+++ src/react-dnd-demo.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import {DndProvider, useDrag, useDrop, DragSourceMonitor} from 'react-dnd';
+import React, {useState} from 'react';
+import {DndProvider, useDrag, useDrop, DragSourceMonitor, DropTargetMonitor} from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
+import {cloneDeep} from 'lodash';
 
 enum ItemType {
     LIST_ITEM = 'listItem'
 };
 
+interface DragObject {
+    type: ItemType,
+    id: number
+}
+
 
 //drop?: (item: DragObject, monitor: DropTargetMonitor) => DropResult | undefined;
+//canDrop?: (item: DragObject, monitor: DropTargetMonitor) => boolean;
+
 
 // see interfaces/hooksApi.d.ts
 
-function ListItem(props: {value: string}) {
+function ListItem(props: {id: number, value: string, onSwap: Function}) {
     const dragSpec = {
-        item: {type: ItemType.LIST_ITEM},
+        item: {type: ItemType.LIST_ITEM, id: props.id},
         collect: (monitor: DragSourceMonitor) => ({
             isDragging: !!monitor.isDragging()
         })
@@ -24,25 +32,56 @@ function ListItem(props: {value: string}) {
     const dropSpec = {
         accept: ItemType.LIST_ITEM,
         drop: (item: any, monitor: any) => {
-            console.log("item is %o", monitor.getItem());
+            props.onSwap(item.id, props.id);
+        },
+        hover: (item: DragObject, monitor: DropTargetMonitor) => {
+            console.log("testing droppability: %o", monitor.canDrop());
+        },
+        canDrop: (item: DragObject, monitor: DropTargetMonitor) => {
+            return false;
         }
     };
 
     const [dropProps, dropTargetRef] = useDrop(dropSpec);
     
-    return <li ref={dropTargetRef}><span ref={dragSourceRef}>{props.value}</span></li>;
+    return (
+        <li ref={dropTargetRef}>
+          <span ref={dragSourceRef}>{props.value}</span>
+        </li>
+    );
 }
 
 export function ReactDndDemo() {
+    const initialData = [
+        {id: 1, value: "Fry"},
+        {id: 2, value: "Bender"},
+        {id: 3, value: "Leela"}
+    ];
+
+    const [data, setData] = useState(initialData);
+    
+
+    function handleSwap(sourceId: number, targetId: number) {
+        console.log("I would handle swap: %o -> %o", sourceId, targetId);
+        const sourcePosition = data.findIndex(x => x.id === sourceId);
+        const targetPosition = data.findIndex(x => x.id === targetId);
+        
+        console.log("swapping position ", sourcePosition, targetPosition);
+
+        const newData = cloneDeep(data);
+        newData[targetPosition] = data[sourcePosition];
+        newData[sourcePosition] = data[targetPosition];
+        setData(newData);
+    }
+
+
     return (
         <DndProvider backend={HTML5Backend}>
           <div>
             <h1>My Component</h1>
 
             <ol>
-              <ListItem value="Fry"/>
-              <ListItem value="Bender"/>
-              <ListItem value="Leela"/>
+              {data.map(x => <ListItem key={x.id} {...x} onSwap={handleSwap}/>)}
             </ol>
           </div>
         </DndProvider>
