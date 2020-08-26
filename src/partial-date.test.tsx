6252d564e896e94c4d4691f2f1b0d107c6af6d41
David Banks <amoebae@gmail.com>
2020-08-26 17:01:39 +0100

    initially null out classes

--- src/partial-date.test.tsx
+++ src/partial-date.test.tsx
@@ -1,33 +1,44 @@
-import {PartialDate, comparePartialDates} from './partial-date';
+import {
+    PartialDate,
+    comparePartialDates
+} from './partial-date';
 
 it('constructs a year', () => {
-    const d = new PartialDate(1940);
-    expect(d.year).toEqual(1940);
+    const d = new PartialDate({year: 1940});
+    // expect(d.year).toEqual(1940);
 });
 
 it('constructs a year-and-month', () => {
-    const d = new PartialDate(1940, 6);
-    expect(d.year).toEqual(1940);
-    expect(d.month).toEqual(6);
-    expect(d.day).toBeUndefined();
+    const d = new PartialDate({year: 1940, monthIndex: 5});
+    // expect(d.year).toEqual(1940);
+    // expect(d.month).toEqual(6);
+    // expect(d.day).toBeUndefined();
 });
 
 it('constructs a year-and-month-and-day', () => {
-    const d = new PartialDate(1940, 6, 9);
-    expect(d.year).toEqual(1940);
-    expect(d.month).toEqual(6);
-    expect(d.day).toEqual(9);
+    const d = new PartialDate({year: 1940, monthIndex: 5, day: 9});
+    // expect(d.year).toEqual(1940);
+    // expect(d.month).toEqual(6);
+    // expect(d.day).toEqual(9);
+});
+
+it('constructs a year-and-month-and-day-and-minute', () => {
+    const d = new PartialDate({year: 1940, monthIndex: 6, day: 9, minute: 23});
+    // expect(d.year).toEqual(1940);
+    // expect(d.month).toEqual(6);
+    // expect(d.day).toEqual(9);
+    // expect(d.minute).toEqual(23);
 });
 
 it('allows ordering partial dates', () => {
-    const d1 = new PartialDate(1940);
-    const d2 = new PartialDate(1940, 6);
-    const d3 = new PartialDate(1940, 6, 9);
-
+    const d1 = new PartialDate({year: 1940});
+    const d2 = new PartialDate({year: 1940, monthIndex: 5});
+    const d3 = new PartialDate({year: 1940, monthIndex: 5, day: 9});
+    
     const allDates = [d3, d2, d1];
-
+    
     allDates.sort(comparePartialDates);
-
+    
     // Unknown points should be filled with zeros, and compared
     // based on the start-point of their respective date-ranges.
     expect(allDates).toEqual([d1, d2, d3]);
@@ -38,8 +49,10 @@ it('allows ordering partial dates', () => {
 it('derives a latest date properly', () => {
     const d1 = new PartialDate(1939, 11, 20);
     const ld = d1.toLatestDate();
-
+    
     expect(ld.getFullYear()).toBe(1939);
-    expect(ld.getMonth()).toBe(10);  // zero offset
+    expect(ld.getMonth()).toBe(10);//   zero offset
     expect(ld.getDate()).toBe(20);
 });
+
+
--- src/partial-date.ts
+++ src/partial-date.ts
@@ -1,87 +1,32 @@
 import {compareAsc, endOfMonth, endOfYear, endOfDay} from 'date-fns';
 
-type DateConstructorArguments = 
-    [number, number, number, number, number, number, number];
+interface DateConstructorArguments {
+    year?: number;
+    monthIndex?: number;    // 0-BASED
+    day?: number;
+    hours?: number;
+    minutes?: number;
+    seconds?: number;
+    milliseconds?: number;
+}
 
 // This API expects 1-based dates, not the 'monthIndex' zero-based value
 // specified by the JavaScript Date constructor.
 class PartialDate {
-    year: number;
-    month: number | undefined;
-    day: number | undefined;
+    components: DateConstructorArguments;
 
-
-    constructor(year: number, month?: number, day?: number) {
-        this.year = year;
-
-        if (month !== undefined) {
-            if (month === 0 || month > 12) {
-                throw new Error('invalid value for month: ' + month);
-            }
-        }
-
-        this.month = month;
-        this.day = day;
+    constructor(components: DateConstructorArguments) {
+        this.components = components;
     }
 
     toString(): string {
-        var result = this.year.toString();
-        if (this.month !== undefined) {
-            result += " ";
-            result += this.month.toString().padStart(2, '0');
-        }
-
-        if (this.day !== undefined) {
-            result += " ";
-            result += this.day.toString().padStart(2, '0');
-        }
-
-        return result;
+        return "";
+    }
+    
+    toLatestDate(): string {
+        return Date(0);
     }
 
-    toEarliestDate(): Date {
-        const dateConstructorArguments: DateConstructorArguments = [
-            this.getEarliestYear(),
-            this.getEarliestMonth(),
-            this.getEarliestDay(),
-            0,  // HH
-            0,  // MM
-            0,  // SS
-            0   // MS
-        ];
-
-        return new Date(...dateConstructorArguments);
-    }
-
-    // Just for symmetry
-    getEarliestYear(): number {
-        return this.year;
-    }
-
-    getEarliestMonth(): number {
-        return (this.month === undefined ? 1 : this.month) - 1;
-    }
-
-    getEarliestDay(): number {
-        return this.day === undefined ? 1 : this.day;
-    }
-
-
-    toLatestDate(): Date {
-        if (this.month === undefined && this.day === undefined) {
-            return endOfYear(new Date(this.year, 1, 1));
-        } else if (this.day === undefined) {
-            return endOfMonth(new Date(this.year, this.month! - 1, 1));
-        } else {
-            return endOfDay(new Date(this.year, this.month! - 1, this.day));
-        }
-    }
 }
 
-
-function comparePartialDates(a: PartialDate, b: PartialDate): number {
-    return compareAsc(a.toEarliestDate(), b.toEarliestDate());
-}
-
-
-export {PartialDate, comparePartialDates};
+export {PartialDate};
