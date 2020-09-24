15027a7b3fdc325e6e7850db990325d2dedca33c
David Banks <amoebae@gmail.com>
2020-09-24 16:19:16 +0100

    specify refetch to update existing stuff

--- src/mutation-client-demo.tsx
+++ src/mutation-client-demo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
     ApolloClient, InMemoryCache, ApolloProvider
 } from '@apollo/client';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000', cache: new InMemoryCache()
@@ -20,6 +20,22 @@ const EVENT_SEQUENCE_QUERY = gql`
         }
     }    
 `;
+
+const EVENT_QUERY = gql`
+    query Bar {
+        Event {
+            description
+        }
+    }
+`
+
+const CREATE_EVENT = gql`
+    mutation {
+        CreateEvent(description: "foo") {
+            description
+        }
+    }
+`
 
 //client.query({query}).then(result => console.log(result));
 
@@ -54,15 +70,40 @@ function EventSequenceView(props: EventSequence) {
     );
 }
 
+function CreateEventWidget() {
+    const [createEvent, {data}] = useMutation(
+        CREATE_EVENT, {refetchQueries: [{query: EVENT_QUERY}]}
+    );
+    
+    return (
+        <div>
+          <button onClick={() => {
+            createEvent();
+          }}>Create event</button>
+        </div>
+    )
+}
+//
 
 function SequenceData() {
     const {loading, error, data} = useQuery(EVENT_SEQUENCE_QUERY);
 
+    const foo = useQuery(EVENT_QUERY);
+
+    
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error!</p>;
 
+    if (foo.loading) return <div/>;
+    if (foo.error) return <div/>;
+
     return (
-        <div>{data['EventSequence'].map((es: EventSequence) => <EventSequenceView key={es.uuid} {...es}/>)}
+        <div>
+          {data['EventSequence'].map((es: EventSequence) => <EventSequenceView key={es.uuid} {...es}/>)}
+
+          <CreateEventWidget/>
+
+          {foo.data['Event'].length}
         </div>
     );
 }
