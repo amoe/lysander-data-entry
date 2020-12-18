eda27cc6aba2047583f3980b5edba0d89ceb87cd
David Banks <amoebae@gmail.com>
2020-12-18 10:19:56 +0000

    finally displaying proper errors

--- src/event-form/construct-link.ts
+++ src/event-form/construct-link.ts
@@ -1,15 +1,19 @@
 import {HttpLink, ApolloLink} from '@apollo/client';
 import { onError } from "@apollo/client/link/error";
+import { GraphQLError } from 'graphql';
+import { notification } from 'antd';
+
+function reportError(error: GraphQLError) {
+    const {message, locations, path} = error;
+    const description = `Message: ${message}, Location: ${locations}, Path: ${path}`;
+    notification.error({message: 'GraphQL error', description, duration: 10});
+}
 
 export function constructLink(uri: string) {
-    const link2 = onError(({ graphQLErrors, networkError }) => {
-        if (graphQLErrors)
-            graphQLErrors.map(
-                ({ message, locations, path }) =>
-                    console.log(
-                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-                    )
-            );
+    const link2 = onError(({graphQLErrors, networkError}) => {
+        if (graphQLErrors) {
+            graphQLErrors.forEach(reportError);
+        }
         if (networkError) console.log(`[Network error]: ${networkError}`);
     });
 
