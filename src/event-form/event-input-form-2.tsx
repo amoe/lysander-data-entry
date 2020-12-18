2c3521707e40bc7fe30487e05394f55b3dae7673
David Banks <amoebae@gmail.com>
2020-12-18 10:57:04 +0000

    need an explicit moment value

--- src/event-form/event-input-form-2.tsx
+++ src/event-form/event-input-form-2.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {EventInputDetails} from './interfaces';
+import {InputNumber, TimePicker} from 'antd';
+import moment from 'moment';
 
 export function EventInputForm(
     props: {
@@ -12,6 +14,19 @@ export function EventInputForm(
         console.log("child: propagating change with value %o", value);
         props.onChange({...props.value, [target.name]: value});
     };
+
+    const f = (val: string | number | undefined) => {
+        if (typeof val === 'string') throw new Error("string");
+        if (val === undefined) throw new Error("undefined");
+        
+        props.onChange(
+            {...props.value, timeOffset: {...props.value.timeOffset, dayOrdinal: val}}
+        );
+    };
+
+    const format = 'HH:mm';
+
+    const defaultValue = moment('12:08', format);
     
     return (
         <div>
@@ -19,6 +34,11 @@ export function EventInputForm(
                  name="description"
                  value={props.value.description}
                  onChange={handleChange}/>
+
+          <InputNumber value={props.value.timeOffset.dayOrdinal}
+                       onChange={f}/>
+
+          <TimePicker defaultValue={defaultValue} format={format} />
         </div>
     );
 }
